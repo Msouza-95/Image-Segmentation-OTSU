dd4dc02945b2f383deca8ef221d3056e54ca6946
Msouza <msdsantos96@gmail.com>
2021-11-08 09:59:02 -0300

    setup experimetal

--- setup/setupExperimental.py
+++ setup/setupExperimental.py
@@ -1,15 +1,14 @@
 import time as t
 
 
-n = 30000000
+n = 10000000
 
-init_time = t.time(); 
+init_time = t.time()
 
 for i in range(0,n):
-    a= 2+1
+    a= 2
 
 end_time = t.time()
-
 time = end_time-init_time
 
 print(time)
