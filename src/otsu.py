7eb5dfea26e01c62e92e73f1063dbb1a3c72e51b
Msouza <msdsantos96@gmail.com>
2021-11-28 22:02:03 -0300

    remoção de comentarios irrelevantes

--- src/otsu.py
+++ src/otsu.py
@@ -8,7 +8,7 @@ from matplotlib import image
 import numpy as np
 from matplotlib import pyplot
 
-imagePath = 'D:/Projetos/Engenharia de Programas/Image-Segmentation-OTSU/Data/mulherBrancaAltaN3.jpg'
+imagePath = 'D:/Projetos/Engenharia de Programas/Image-Segmentation-OTSU/Data/MulherBrancaN4.jpg'
 img = image.imread(imagePath)
 #precisa realizar a leitura dos dados
 
@@ -16,37 +16,35 @@ n = int(img.size/3)
 R, G , B = np.reshape(img[:,:,0], n), np.reshape(img[:,:,1],n),np.reshape(img[:,:,2],n)
 C = [0]*n
 M = 256
-# print("Digite a quntidade de Classes ")
-# K = int(input())
 K = 2
 
 #inicialização
 init_time = t.time()
+
 #Transformação da imagem colorida em preto-e-branco como descrito acima, atribuindo a
 #cada pixel da imagem uma cor entre 0 e 255.
 for i in range(0,n-1):
     C[i]=int(0.2989*R[i]+0.5870*G[i]+0.1140*B[i])
 
-#pyplot.imshow(C)
+
 #Calcular o número de pixels de cada cor de 0 a 255 (histograma p)
 tmP = M-1 # tamnho de P
 p = [0] * tmP  #inicializando com 0 todas as cores
-#varrendo os pixels contando o numero de cada cor
 
+#varrendo os pixels contando o numero de cada cor
 for i in range(0, n-1):
     p[C[i]]+=1
 
 #normalizando o histograma
-
 for j in range(0, M-1):
     p[j]=p[j]/n
 
 stop_time = t.time()
 tmpInit= round(stop_time - init_time,4)
+
 #processo iterativo
 
 iterativo_time = t.time()
-
 Qmax =0
 iter =0
 
@@ -98,10 +96,6 @@ print("tempo de inicialização", tmpInit )
 print('Iteracao',iter)
 print("tempo por iteração ",tempoPorIteracao)
 print("#----------------------------------------#")
-
-
-
-
 
 newImage = newImg.createImage(imagePath, fMax)
 pyplot.imshow(newImage)
